603bf77b3316570c0038e6082d6c89a06383453a
W-David <1776867536@qq.com>
2022-12-18 22:22:18 +0800

    完善移动端界面

---
+++ src/components/SwitchDark.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="switch-dark-container">
+    <el-switch
+      size="large"
+      :model-value="modelValue"
+      @change="change"
+      inline-prompt
+      :active-icon="'i-moon'"
+      :inactive-icon="'i-sunny'"
+      style="--el-switch-on-color: #1d1d1d; --el-switch-off-color: #d1d1d1"
+    />
+  </div>
+</template>
+
+<script setup>
+import { defineProps, toRefs, defineEmits } from 'vue'
+const props = defineProps({
+  modelValue: Boolean
+})
+const { modelValue } = toRefs(props)
+const emit = defineEmits(['update:modelValue'])
+const change = val => emit('update:modelValue', val)
+</script>
+
+<style lang="scss" scoped>
+.switch-dark-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/layout/components/NavBar.vue
+++ src/layout/components/NavBar.vue
@@ -4,7 +4,7 @@
       <el-icon :size="20" class="hidden-md-and-up" @click.stop.prevent="handleExpand">
         <i-expand />
       </el-icon>
-      Cody's blog
+      <span class="icon-text">Cody's blog</span>
     </div>
     <div v-if="isUserLogin || isAdminLogin" class="nav-menu hidden-sm-and-down">
       <el-menu :default-active="activePage" :router="true" mode="horizontal" ref="menuRef" :ellipsis="false">
@@ -13,28 +13,11 @@
         </el-menu-item>
       </el-menu>
     </div>
-    <div v-if="isUserLogin || isAdminLogin" class="nav-search hidden-md-and-down">
-      <el-autocomplete
-        v-model="queryText"
-        :fetch-suggestions="querySearch"
-        clearable
-        placeholder="标题 / 标签 / 类别"
-        @select="handleQuery"
-      >
-        <template #prefix>
-          <el-icon @click="handleQuery"><i-search /></el-icon>
-        </template>
-      </el-autocomplete>
+    <div v-if="isUserLogin || isAdminLogin" class="nav-search">
+      <search></search>
     </div>
-    <div class="nav-switch">
-      <el-switch
-        size="large"
-        v-model="isDark"
-        inline-prompt
-        :active-icon="'i-moon'"
-        :inactive-icon="'i-sunny'"
-        style="--el-switch-on-color: #1d1d1d; --el-switch-off-color: #d1d1d1"
-      />
+    <div class="nav-switch hidden-xs-only">
+      <switch-dark v-model="isDark"></switch-dark>
     </div>
     <div class="nav-user-info hidden-sm-and-down">
       <user v-if="isUserLogin"></user>
@@ -53,8 +36,8 @@
       :z-index="10000"
       @close="onCloseDrawer"
     >
-      <div class="collapse-container">
-        <el-icon class="mb m-collapse" :size="20" @click.stop.prevent="handleCollapse">
+      <div class="collapse-top">
+        <el-icon class="drawer-icon" :size="20" @click.stop.prevent="handleCollapse">
           <i-fold />
         </el-icon>
         <div class="drawer-user-info">
@@ -71,122 +54,73 @@
           mode="veritical"
           @select="handleChange"
         >
+          <el-menu-item class="hidden-md-and-up" index="/blog">博文</el-menu-item>
           <el-menu-item v-for="menu in menuList" :key="menu.id" :index="menu.path">
             {{ menu.name }}
           </el-menu-item>
         </el-menu>
       </div>
-      <el-button class="m-logout" type="info" size="small" plain @click="handleLogout"> 退出登录 </el-button>
+      <div class="collapse-bottom">
+        <el-button class="drawer-logout" type="warning" plain @click="handleLogout"> 退出登录 </el-button>
+        <switch-dark class="drawer-switch hidden-sm-and-up" v-model="isDark"></switch-dark>
+      </div>
     </el-drawer>
   </div>
 </template>
 
-<script>
-import { reactive, ref, toRef, toRefs, computed } from 'vue'
+<script setup>
+import { defineProps, reactive, ref, toRef, toRefs, computed } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter, useRoute } from 'vue-router'
 import User from '@/components/User'
 import Admin from '@/components/Admin'
+import Search from '@/components/Search'
+import SwitchDark from '@/components/SwitchDark'
 import { getUserType } from '@/util/auth'
 import { useDark } from '@vueuse/core'
 
-export default {
-  name: 'HNavBar',
-  components: {
-    User,
-    Admin
+const props = defineProps({
+  activePage: {
+    type: String,
+    required: true
   },
-  props: {
-    activePage: {
-      type: String,
-      required: true
-    },
-    menuList: {
-      type: Array,
-      required: true
-    }
-  },
-  setup(props, { attrs, slots, emit, expose }) {
-    const userType = getUserType()
-    const store = useStore()
-    const router = useRouter()
-    const route = useRoute()
-    const userAvatar = computed(() => store.getters.userAvatar)
-    const adminAvatar = computed(() => store.getters.adminAvatar)
-    const device = computed(() => store.getters.device)
-    const loginInfo = computed(() => store.getters.loginInfo)
-    const queryText = ref('')
-    const queryResList = ref([])
-    const sidebarOpen = computed(() => store.getters.sidebarOpen)
-    const setSidebarOpen = open => store.dispatch('app/ToggleSidebar', open)
-    const menuRef = ref()
-
-    const isDark = useDark()
-
-    const isUserLogin = computed(() => store.getters.isUserLogin)
-    const isAdminLogin = computed(() => store.getters.isAdminLogin)
-
-    const handleChange = () => setSidebarOpen(false)
-    const handleExpand = e => setSidebarOpen(true)
-    const handleCollapse = () => setSidebarOpen(false)
-    const onCloseDrawer = () => setSidebarOpen(false)
-
-    const handleInfo = () => {}
-    const handleLogout = async () => {
-      handleChange()
-      if (userType === 'user') {
-        store.commit('user/LOGOUT')
-        router.push({ name: 'login' })
-      } else if (userType === 'admin') {
-        store.commit('admin/LOGOUT')
-        router.push({ name: 'login' })
-      } else {
-        router.push({ name: 'login' })
-      }
-    }
-
-    const handleQuery = ({ type, id }) => router.push({ name: type, params: { id } })
-    const createFilter = qs => restaurant => restaurant.value.toLowerCase().indexOf(qs.toLowerCase()) === 0
-    const getQueryResList = async qs => {
-      await new Promise(rs => setTimeout(rs, 1500))
-      queryResList.value = [
-        { value: 'NavelAI Surfure', type: 'article', id: '976' },
-        { value: '测试中文标题', type: 'article', id: '594' },
-        { value: 'Linux', type: 'category', id: '4' },
-        { value: '学习', type: 'tag', id: '8' }
-      ]
-    }
-    const querySearch = async (qs, callback) => {
-      await getQueryResList(qs)
-      const results = qs ? queryResList.value.filter(createFilter(qs)) : queryResList.value
-      callback(results)
-    }
-    const toLogin = () => {
-      router.push({ name: 'login' })
-    }
-
-    return {
-      userAvatar,
-      adminAvatar,
-      isUserLogin,
-      isAdminLogin,
-      loginInfo,
-      menuRef,
-      device,
-      queryText,
-      handleQuery,
-      querySearch,
-      isDark,
-      sidebarOpen,
-      handleChange,
-      handleExpand,
-      handleCollapse,
-      handleInfo,
-      onCloseDrawer,
-      handleLogout,
-      toLogin
-    }
+  menuList: {
+    type: Array,
+    required: true
   }
+})
+const { activePage, menuList } = toRefs(props)
+const userType = getUserType()
+const store = useStore()
+const router = useRouter()
+const sidebarOpen = computed(() => store.getters.sidebarOpen)
+const setSidebarOpen = open => store.dispatch('app/ToggleSidebar', open)
+const menuRef = ref()
+
+const isDark = useDark()
+
+const isUserLogin = computed(() => store.getters.isUserLogin)
+const isAdminLogin = computed(() => store.getters.isAdminLogin)
+
+const handleChange = () => setSidebarOpen(false)
+const handleExpand = e => setSidebarOpen(true)
+const handleCollapse = () => setSidebarOpen(false)
+const onCloseDrawer = () => setSidebarOpen(false)
+
+const handleLogout = async () => {
+  handleChange()
+  if (userType === 'user') {
+    store.commit('user/LOGOUT')
+    router.push({ name: 'login' })
+  } else if (userType === 'admin') {
+    store.commit('admin/LOGOUT')
+    router.push({ name: 'login' })
+  } else {
+    router.push({ name: 'login' })
+  }
+}
+const toLogin = () => {
+  router.push({ name: 'login' })
 }
 </script>
 
@@ -201,8 +135,11 @@ export default {
   .nav-icon {
     @include flex-box(row, space-between, center);
     @include font-hei;
-    font-size: 18px;
     font-weight: bolder;
+    .icon-text {
+      font-size: 18px;
+    }
+
     .el-icon {
       margin-right: 6px;
       &:active {
@@ -233,19 +170,17 @@ export default {
   }
   .nav-search {
     margin-left: auto;
-    margin-right: 16px;
-    &:deep {
-      .el-input {
-        width: 320px;
-      }
-    }
   }
 
   .nav-switch {
-    margin-right: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 16px;
   }
 
   .nav-user-info {
+    margin-left: 16px;
     .to-login {
       font-weight: bolder;
       color: $font-color-a;
@@ -255,4 +190,3 @@ export default {
   }
 }
 </style>
->
