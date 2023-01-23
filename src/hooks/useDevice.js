4d78805fa9df77352cd8b671a5bfd18c5a03c530
W-David <1776867536@qq.com>
2023-01-23 22:31:49 +0800

    修复暗黑模式异常问题

--- src/hooks/useDevice.js
+++ src/hooks/useDevice.js
@@ -5,7 +5,6 @@ import { device } from 'device.js'
 const useDevice = () => {
   onMounted(() => $_get_device())
   const $_get_device = () => {
-    device.addClasses(document.documentElement)
     const isTouchDevice = device.mobile || device.tablet
     store.commit('app/SET_ISTOUCHDEVICE', !!isTouchDevice)
   }
